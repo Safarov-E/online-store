ac034aef0be89366bc7b38f0dcb74504e862fece
Safarov <elkhan.safarov10@yandex.com>
2020-06-06 20:54:09 +0500

    Создание навигационного меню

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-app-bar dark color="primary">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Online Store</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn text>Link One</v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify';
+import Router from 'vue-router'
+import router from './router'
 
+Vue.use(Router)
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify : new Vuetify(),
+  router,
+  render: h => h(App)
 }).$mount('#app')
