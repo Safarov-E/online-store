822e1122d7219efeed878e4adbe899f276c7398c
Safarov <elkhan.safarov10@yandex.com>
2020-06-08 19:59:43 +0500

    Подключение и настройка Vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import Router from 'vue-router'
 import router from './router'
+import store from './store'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Router)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   vuetify : new Vuetify(),
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
