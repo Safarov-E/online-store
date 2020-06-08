5d5b7977b6fd7cb687fa20303ddb1a2d41f5c763
Safarov <elkhan.safarov10@yandex.com>
2020-06-08 14:13:29 +0500

    Реализация страницы - Заказов

--- src/components/User/Checkout.vue
+++ src/components/User/Checkout.vue
@@ -1,8 +1,44 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Checkout</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text-secondary mb-3">Orders</h1>
+        <v-card 
+            class="mx-auto"
+        >
+                <v-list
+                subheader
+                two-line
+                flat
+                >
+
+                <v-list-item-group 
+                    multiple
+                >
+                    <v-list-item
+                        v-for="(order, i) in orders"
+                        :key="i"
+                    >
+                        <v-list-item-action>
+                        <v-checkbox 
+                            :input-value="order.done"
+                            color="success"
+                            @change="markDone(order)"
+                        ></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{ order.name }}</v-list-item-title>
+                            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-btn 
+                            :to="'/product/' + order.productId"
+                            class="primary"
+                        >Open</v-btn>
+                    </v-list-item>
+                </v-list-item-group>
+                </v-list>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,7 +46,22 @@
 <script>
 export default {
     data() {
-        return {}
+        return {
+            orders: [
+                {
+                    id: 'text',
+                    name: 'Konstantin',
+                    phone: '22-22-22',
+                    productId: '1',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        markDone (order) {
+            order.done = true
+        }
     }
 }
 </script>
