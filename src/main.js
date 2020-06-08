d6a998277488a406ded6aad9b317035dd2d592fe
Safarov <elkhan.safarov10@yandex.com>
2020-06-08 22:19:10 +0500

    Подключение и настройка Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vuetify from 'vuetify'
 import Router from 'vue-router'
 import router from './router'
 import store from './store'
+import * as fb from 'firebase/app'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Router)
@@ -14,5 +15,18 @@ new Vue({
   vuetify : new Vuetify(),
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCV63zuyM6aEU30j1dbmGqIEf9z-_crH2Y",
+      authDomain: "onlinestore-7794e.firebaseapp.com",
+      databaseURL: "https://onlinestore-7794e.firebaseio.com",
+      projectId: "onlinestore-7794e",
+      storageBucket: "onlinestore-7794e.appspot.com",
+      messagingSenderId: "862877312535",
+      appId: "1:862877312535:web:1a096fcd83b8e7d0629e52",
+      measurementId: "G-H72RP0GGVY"
+    }
+    fb.initializeApp(firebaseConfig)
+  }
 }).$mount('#app')
